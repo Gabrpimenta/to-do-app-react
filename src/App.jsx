6a4e8ddf153007048b3e6dbace09f480f38579fc
Gabriel Pimenta <gabrielrpimenta@gmail.com>
2023-03-27 20:25:17 -0300

    🚧 work in progress TaskList and TaskItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import CustomForm from './components/CustomForm';
+import TaskList from './components/TaskList';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
   const addTask = (task) => {
-    console.log(task);
+    setTasks((prevTasks) => {
+      return [...prevTasks, task];
+    });
   };
+
   return (
     <div className='container'>
       <header>
         <h1>My Task List</h1>
       </header>
       <CustomForm addTask={addTask} />
+      {tasks && <TaskList tasks={tasks} />}
     </div>
   );
 }
